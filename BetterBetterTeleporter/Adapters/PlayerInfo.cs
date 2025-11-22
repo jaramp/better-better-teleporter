f4e270e632b024fd168e0fc64b6ae0b9a1fe8c7e
jaramp <jaramp>
2025-11-22 15:50:17 -0500

    Cleaning up PlayerInfo

--- BetterBetterTeleporter/Adapters/PlayerInfo.cs
+++ BetterBetterTeleporter/Adapters/PlayerInfo.cs
@@ -13,8 +13,7 @@ public interface IPlayerInfo
 
 public sealed class PlayerInfo(PlayerControllerB player) : IPlayerInfo
 {
-    private IReadOnlyList<IItemInfo> _slots = [.. player.ItemSlots.Select(item => item == null ? null : new ItemInfo(item))];
+    private readonly IReadOnlyList<IItemInfo> _slots = [.. player.ItemSlots.Select(item => item == null ? null : new ItemInfo(item))];
     public IReadOnlyList<IItemInfo> Slots => _slots;
-    private readonly int _currentSlotIndex = player.currentItemSlot;
-    public int CurrentSlotIndex => _currentSlotIndex;
+    public int CurrentSlotIndex => player.currentItemSlot;
 }
